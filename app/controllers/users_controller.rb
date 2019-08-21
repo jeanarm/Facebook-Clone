619a0764b48518bd631a6e91edf30e3e1868b990
jeanarm <nizigiarmel2015@gmail.com>
2019-08-21 10:17:28 +0200

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to sessions_new_path, notice: 'User was successfully created.' }
+        format.html { redirect_to new_sessions_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
